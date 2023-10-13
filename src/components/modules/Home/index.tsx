d6dcf9eb09ad8eeed0ba96e015195033424faaf7
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-10-13 10:10:29 +0700

    feat: update

--- src/components/modules/Home/index.tsx
+++ src/components/modules/Home/index.tsx
@@ -3,8 +3,10 @@ import React from "react";
 import Introduction from "./components/Introduction";
 import CareerList from "./components/CareerList";
 import dynamic from "next/dynamic";
-const Projects = dynamic(() => import("./components/Projects"));
-const Skills = dynamic(() => import("./components/Skills"));
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+// const Projects = dynamic(() => import("./components/Projects"));
+// const Skills = dynamic(() => import("./components/Skills"));
 type Props = {};
 
 const HomeModules = (props: Props) => {
