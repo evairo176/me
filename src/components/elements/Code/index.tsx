5d7b8e0087687f038c70448c7f701c7f5aa86dcf
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-10-22 23:11:44 +0700

    feat: update again

--- src/components/elements/Code/index.tsx
+++ src/components/elements/Code/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import { BiCopyAlt } from "react-icons/bi";
+import { BsCheck2Circle } from "react-icons/bs";
 import { Button } from "@/components/ui/button";
 
 type CodeProps = {
@@ -10,15 +11,31 @@ type CodeProps = {
 };
 
 const Code = ({ children, language }: CodeProps) => {
-  const [copy, setCope] = useState<boolean>(false);
-  console.log(language);
+  const [copy, setCopy] = useState<boolean>(false);
+
   return (
     <div className="rounded-md border bg-gray-500">
       <div className="flex justify-between items-center p-2 ">
         <div className="text-sm text-white">Example Code</div>
-        <Button variant={"ghost"} className="group">
-          <BiCopyAlt className="text-white w-5 h-5 group-hover:text-gray-500" />
-        </Button>
+        {copy ? (
+          <Button disabled={true} variant={"ghost"} className="group">
+            <BsCheck2Circle className="text-white w-5 h-5 group-hover:text-gray-500" />
+          </Button>
+        ) : (
+          <Button
+            onClick={() => {
+              navigator.clipboard.writeText(children);
+              setCopy(true);
+              setTimeout(() => {
+                setCopy(false);
+              }, 3000);
+            }}
+            variant={"ghost"}
+            className="group"
+          >
+            <BiCopyAlt className="text-white w-5 h-5 group-hover:text-gray-500" />
+          </Button>
+        )}
       </div>
       <SyntaxHighlighter language="javascript" style={atomOneDark}>
         {children}
