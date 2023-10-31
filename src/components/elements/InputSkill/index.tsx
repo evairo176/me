303a2da2face05c8b9328bff169829c7eba0953e
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-10-31 09:25:38 +0700

    feat: update

--- src/components/elements/InputSkill/index.tsx
+++ src/components/elements/InputSkill/index.tsx
@@ -47,12 +47,13 @@ const InputSkill: FC<InputSkillProps> = ({ form, name, label }) => {
 
   useEffect(() => {
     const val = form.getValues(name);
-    console.log({ val });
 
     if (val && val.length > 0) {
       setValues(val);
     }
   }, [form, name]);
+
+  console.log({ values });
 
   return (
     <FormField
@@ -85,29 +86,30 @@ const InputSkill: FC<InputSkillProps> = ({ form, name, label }) => {
                 </div>
               )}
               <div className="space-x-3">
-                {values.map((item: string, key: number) => (
-                  <Badge
-                    variant={"outline"}
-                    key={key}
-                    onClick={() => handleDeleteValue(item)}
-                  >
-                    {item}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-4 h-4 ml-2"
+                {values &&
+                  values?.map((item: string, key: number) => (
+                    <Badge
+                      variant={"outline"}
+                      key={key}
+                      onClick={() => handleDeleteValue(item)}
                     >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
-                  </Badge>
-                ))}
+                      {item}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-4 h-4 ml-2"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </Badge>
+                  ))}
               </div>
             </>
           </FormControl>
