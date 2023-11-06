2583a304f2b39b5fc7a8b03587ada051aa5d690e
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-11-06 15:44:26 +0700

    feat: update

--- src/lib/getDictionaries.tsx
+++ src/lib/getDictionaries.tsx
@@ -1,14 +1,11 @@
-import "server-only";
 import { i18n } from "../../i18n.config";
 
-const dictionaries = {
+const dictionaries: any = {
   en: () =>
     import("../constans/language/en.json").then((module) => module.default),
   id: () =>
     import("../constans/language/id.json").then((module) => module.default),
 };
 
-export const getDictionary = async (locale: "en" | "id") =>
-  dictionaries[
-    i18n.locales.includes(locale) ? locale : (i18n.defaultLocale as "en" | "id")
-  ](); // Add type assertion here
+export const getDictionary = async (locale: string) =>
+  dictionaries[i18n.locales.includes(locale) ? locale : i18n.defaultLocale]();
