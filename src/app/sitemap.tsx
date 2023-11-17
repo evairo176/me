4d420b38a60ecdb7d73864b2e78d2ad7c0b4bb29
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-11-17 13:44:27 +0700

    feat: update sitemap fix

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -5,7 +5,7 @@ import { MetadataRoute } from "next";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseURL = process.env.NEXT_PUBLIC_SITE_URL as string;
 
-  // Get Posts
+  // Get all Posts
   const allBlog = await getAllBlog({ lang: "" });
   const dataBlog = allBlog?.blog;
   //   console.log(data);
