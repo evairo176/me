02e41df75dbc9d0f919160fb810ae8c22881df95
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-11-22 09:33:32 +0700

    feat: update fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -46,6 +46,8 @@ export const authOption: NextAuthOptions = {
               token: response.data.token,
               refreshToken: response.data.refreshToken,
             };
+          } else {
+            return null;
           }
         } catch (error: any) {
           throw new Error(error.response.data.message);
